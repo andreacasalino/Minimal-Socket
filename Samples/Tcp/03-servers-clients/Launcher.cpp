0f82d9f1e11fe3ab3b50ba8715f345d21f68ac5a
Andrea <andrecasa91@gmail.com>
2021-02-07 17:02:59 +0100

    fixing samples

---
+++ Samples/Tcp/03-servers-clients/Launcher.cpp
@@ -0,0 +1,12 @@
+#include <ProcessLauncher.h>
+
+int main() {
+
+   Launcher lnc("launcher");
+   lnc.addProcess("Sample03_Server");
+   lnc.addProcess("Sample03_Client", "500");
+   lnc.addProcess("Sample03_Client", "50");
+   lnc();
+
+   return 0;
+}
