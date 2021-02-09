7cc7c25bb2313e2cd60fb3779bfa4d8ce127dcec
Andrea <andrecasa91@gmail.com>
2021-02-09 22:42:38 +0100

    udp samples restructured

---
+++ Samples/Udp/Launcher-01-client-client.cpp
@@ -0,0 +1,11 @@
+#include "../ProcessLauncher.h"
+
+int main() {
+
+   Launcher lnc("Launcher01");
+   lnc.addProcess("UdpClientAsker","15000","250","10000");
+   lnc.addProcess("UdpClientResponder","10000", "15000");
+   lnc();
+
+   return EXIT_SUCCESS;
+}
