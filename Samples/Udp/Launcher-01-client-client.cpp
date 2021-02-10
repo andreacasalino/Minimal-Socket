2e375f82f7628b749fb2551d120f58225024578f
Andrea <andrecasa91@gmail.com>
2021-02-10 19:35:55 +0100

    samples working but not robustly

--- Samples/Udp/Launcher-01-client-client.cpp
+++ Samples/Udp/Launcher-01-client-client.cpp
@@ -3,8 +3,8 @@
 int main() {
 
    Launcher lnc("Launcher01");
+   lnc.addProcess("UdpClientResponder", "10000", "15000");
    lnc.addProcess("UdpClientAsker","15000","250","10000");
-   lnc.addProcess("UdpClientResponder","10000", "15000");
    lnc();
 
    return EXIT_SUCCESS;
