ba596b42dc13c8cb3f1423fea2ea22de8aaca155
Andrea <andrecasa91@gmail.com>
2021-02-10 23:44:44 +0100

    sleep in process launcher added

--- Samples/Tcp/Launcher-01-server-client.cpp
+++ Samples/Tcp/Launcher-01-server-client.cpp
@@ -3,8 +3,8 @@
 int main() {
 
    Launcher lnc("Launcher01");
-   lnc.addProcess("TcpServer","20000");
-   lnc.addProcess("TcpClient","20000");
+   lnc.addProcess("TcpServer", "20000");
+   lnc.addProcessSleep(std::make_pair("TcpClient", 1), "20000");
    lnc();
 
    return EXIT_SUCCESS;
