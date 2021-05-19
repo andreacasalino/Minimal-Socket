a0f0bff0836891e91dd37786e53801c5ce06176e
Andrea <andrecasa91@gmail.com>
2021-05-19 13:09:31 +0200

    implementing tests

--- CrossSocket/TypedSocket/include/core/TypedAsyncMessanger.h
+++ CrossSocket/TypedSocket/include/core/TypedAsyncMessanger.h
@@ -1,49 +1,55 @@
-///**
-// * Author:    Andrea Casalino
-// * Created:   01.28.2020
-// *
-// * report any bug to andrecasa91@gmail.com.
-// **/
-//
-//#ifdef ASYNCH_ENABLED
-//#ifndef _CROSS_SOCKET_TYPEDASYNCMESSANGER_H_
-//#define _CROSS_SOCKET_TYPEDASYNCMESSANGER_H_
-//
-//#include <messanger/AsyncMessanger.h>
-//#include <core/components/Decoder.h>
-//#include <core/TypedSender.h>
-//#include <core/TypedMessangerListener.h>
-//
-//namespace sck::typed {
-//    class TypedAsynchMessanger
-//        : protected async::AsyncMessanger
-//        , protected async::MessangerListener
-//        , public async::Talker<TypedMessangerListener<RecvT>>
-//        , public TypedSender<SendT, Encoder_>
-//        , protected Decoder_ {
-//    public:
-//        TypedAsynchMessanger(std::unique_ptr<Connection> messanger, const std::size_t& bufferCapacity)
-//            : AsyncMessanger(std::move(messanger), bufferCapacity) {
-//            this->async::MessageTalker::resetListener(this);
-//        };
-//
-//        inline bool isOpen() const { return this->async::AsyncMessanger::isOpen(); };
-//
-//        inline void close() { this->async::AsyncMessanger::close(); };
-//
-//        inline void open(const std::chrono::milliseconds& timeout) { this->async::AsyncMessanger::open(timeout); };
-//
-//        inline void resetErrorListener(async::ErrorListener* listener) { this->async::AsyncMessanger::resetListener(listener); };
-//
-//    protected:
-//        void handle(const std::pair<const char*, std::size_t>& message) final {
-//            // bool decode(const std::string& buffer, T& message)
-//            RecvT typed;
-//            this->Decoder_::decode(std::string(message.first, message.second), typed);
-//            this->notify(typed);
-//        }
-//    };
-//}
-//
-//#endif
-//#endif
+/**
+ * Author:    Andrea Casalino
+ * Created:   01.28.2020
+ *
+ * report any bug to andrecasa91@gmail.com.
+ **/
+
+#ifdef ASYNCH_ENABLED
+#ifndef _CROSS_SOCKET_TYPEDASYNCMESSANGER_H_
+#define _CROSS_SOCKET_TYPEDASYNCMESSANGER_H_
+
+#include <messanger/AsyncMessanger.h>
+#include <core/TypedSender.h>
+#include <core/TypedMessangerListener.h>
+#include <core/components/Decoder.h>
+
+namespace sck::typed {
+    template<typename SendT, typename Encoder_, typename RecvT, typename Decoder_>
+    class TypedAsynchMessanger
+        : public StateAware
+        , public Openable
+        , public Closable
+        , protected async::MessangerListener
+        , public TypedSender<SendT, Encoder_>
+        , public async::Talker<TypedMessangerListener<RecvT>>
+        , protected Decoder_ {
+    public:
+        TypedAsynchMessanger(std::unique_ptr<Connection> messanger, const std::size_t& bufferCapacity)
+            : asyncMessanger(std::move(messanger), bufferCapacity) {
+            this->sender = &this->asyncMessanger;
+        };
+
+        inline bool isOpen() const override { return this->asyncMessanger.isOpen(); };
+
+        inline void close() override { this->asyncMessanger.close(); };
+
+        inline void open(const std::chrono::milliseconds& timeout) override { this->asyncMessanger.open(timeout); };
+
+        inline void resetErrorListener(async::ErrorListener* listener) { this->asyncMessanger.resetListener(listener); };
+
+    protected:
+        void handle(const std::pair<const char*, std::size_t>& message) final {
+            // bool decode(const std::string& buffer, T& message)
+            RecvT typed;
+            this->Decoder_::decode(std::string(message.first, message.second), typed);
+            this->notify(typed);
+        }
+
+    private:
+        async::AsyncMessanger asyncMessanger;
+    };
+}
+
+#endif
+#endif
--- Tests/Test03-typed.cpp
+++ Tests/Test03-typed.cpp
@@ -6,7 +6,9 @@ using namespace sck::tcp;
 
 #include <NamesMap.h>
 #include <core/TypedMessanger.h>
+#include <core/TypedAsyncMessanger.h>
 #include <NamesCoding.h>
+#include <Logger.h>
 using namespace sck::typed;
 
 sample::Names getNames() {
@@ -29,7 +31,24 @@ sample::Names getSurnames() {
     return surnames;
 };
 
-TEST(TcpTyped, ClientAsker_ServerResponder) {
+sample::Names getResponse(const sample::Names& request) {
+    sample::Names response;
+    for (auto it = request.begin(); it != request.end(); ++it) {
+        response.push_back(sample::NamesMap::getSurname(*it));
+    }
+    return response;
+};
+
+void doRequests(TypedMessanger<sample::Names, sample::NamesEncoder, sample::Names, sample::NamesDecoder>& messanger, const std::size_t cycles) {
+    for (std::size_t k = 0; k < cycles; ++k) {
+        EXPECT_TRUE(messanger.send(getNames()));
+        sample::Names response;
+        EXPECT_TRUE(messanger.receive(response, std::chrono::milliseconds(0)));
+        EXPECT_EQ(response, getSurnames());
+    }
+}
+
+TEST(SyncTyped, ClientAsker_ServerResponder) {
     const std::uint16_t port = sample::PortFactory::makePort();
     const std::size_t cycles = 5;
 
@@ -43,11 +62,7 @@ TEST(TcpTyped, ClientAsker_ServerResponder) {
             for (std::size_t k = 0; k < cycles; ++k) {
                 sample::Names request;
                 EXPECT_TRUE(messanger.receive(request, std::chrono::milliseconds(0)));
-                sample::Names response;
-                for (auto it = request.begin(); it != request.end(); ++it) {
-                    response.push_back(sample::NamesMap::getSurname(*it));
-                }
-                EXPECT_TRUE(messanger.send(response));
+                EXPECT_TRUE(messanger.send(getResponse(request)));
             }
 #pragma omp barrier
         }
@@ -56,13 +71,61 @@ TEST(TcpTyped, ClientAsker_ServerResponder) {
             std::unique_ptr<TcpClient> client = std::make_unique<TcpClient>(*sck::Ip::createLocalHost(port));
             sample::openTcpClient(*client);
             TypedMessanger<sample::Names, sample::NamesEncoder, sample::Names, sample::NamesDecoder> messanger(std::move(client), 2500);
-            // exchange typed messages
-            for (std::size_t k = 0; k < cycles; ++k) {
-                EXPECT_TRUE(messanger.send(getNames()));
-                sample::Names response;
-                EXPECT_TRUE(messanger.receive(response, std::chrono::milliseconds(0)));
-                EXPECT_EQ(response, getSurnames());
-            }
+            doRequests(messanger, cycles);
+#pragma omp barrier
+        }
+    }
+}
+
+
+class TypedAsyncResponder
+    : public TypedAsynchMessanger<sample::Names, sample::NamesEncoder, sample::Names, sample::NamesDecoder>
+    , public TypedMessangerListener<sample::Names>
+    , protected sck::async::ErrorListener
+    , public sample::Logger {
+public:
+    TypedAsyncResponder(std::unique_ptr<Connection> messanger, const std::size_t& bufferCapacity) 
+        : TypedAsynchMessanger<sample::Names, sample::NamesEncoder, sample::Names, sample::NamesDecoder>(std::move(messanger), bufferCapacity)
+        , Logger("TypedAsyncResponder") {
+        this->resetErrorListener(this);
+        this->resetListener(this);
+    };
+
+private:
+    void handle(const sample::Names& message) final {
+        sample::Names response = getResponse(message);
+        this->send(response);
+        //std::string recStr(message.first, message.second);
+        //const std::string surname = NamesMap::getSurname(recStr);
+        //this->log("request: ", recStr, " response: ", surname);
+        //this->messPtr->send({ surname.data(), surname.size() });
+    };
+
+    void handle(const sck::Error& error) final {};
+
+    void handle(const std::exception& error) final {};
+};
+
+TEST(AsyncTyped, ClientAsker_ServerResponder) {
+    const std::uint16_t port = sample::PortFactory::makePort();
+    const std::size_t cycles = 5;
+
+#pragma omp parallel num_threads(2)
+    {
+        if (0 == omp_get_thread_num()) {
+            // server
+            auto acceptedClient = sample::accept(port);
+            TypedAsynchMessanger<sample::Names, sample::NamesEncoder, sample::Names, sample::NamesDecoder> asynchResponder(std::move(acceptedClient), 2500);
+            open(asynchResponder);
+#pragma omp barrier
+            asynchResponder.close();
+        }
+        else {
+            // client
+            std::unique_ptr<TcpClient> client = std::make_unique<TcpClient>(*sck::Ip::createLocalHost(port));
+            sample::openTcpClient(*client);
+            TypedMessanger<sample::Names, sample::NamesEncoder, sample::Names, sample::NamesDecoder> messanger(std::move(client), 2500);
+            doRequests(messanger, cycles);
 #pragma omp barrier
         }
     }
