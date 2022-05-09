3c46f8e29deae62a598e7ce022e36efe11987282
AndreaCasalino <andrea.casalino@cloudnc.com>
2022-05-09 16:30:17 +0100

    testing

--- tests/TestTCP.cpp
+++ tests/TestTCP.cpp
@@ -51,7 +51,7 @@ void send_response(const SenderReceiver &requester,
 
 TEST_CASE("Establish tcp connection", "[tcp]") {
   const auto port = PortFactory::makePort();
-  const auto family = IP_V6; // GENERATE(IP_V4, IP_V6);
+  const auto family = GENERATE(IP_V4, IP_V6);
 
   std::unique_ptr<TcpConnection> server_side;
   std::unique_ptr<TcpClient> client_side;
@@ -97,7 +97,7 @@ TEST_CASE("Establish tcp connection", "[tcp]") {
 
 TEST_CASE("Establish many tcp connections to same server", "[tcp]") {
   const auto port = PortFactory::makePort();
-  const auto family = IP_V4; // GENERATE(IP_V4, IP_V6);
+  const auto family = GENERATE(IP_V4, IP_V6);
 
   TcpServer server(port, family);
   server.open();
@@ -142,7 +142,7 @@ TEST_CASE("Establish many tcp connections to same server", "[tcp]") {
 
 TEST_CASE("Open multiple times tcp clients", "[tcp]") {
   const auto port = PortFactory::makePort();
-  const auto family = IP_V4; // GENERATE(IP_V4, IP_V6);
+  const auto family = GENERATE(IP_V4, IP_V6);
 
   TcpServer server(port, family);
   server.open();
