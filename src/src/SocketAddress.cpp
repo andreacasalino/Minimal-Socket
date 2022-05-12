fb088a5c881d06fe9ec20aa3b9b627510107572e
AndreaCasalino <andrea.casalino@cloudnc.com>
2022-05-12 17:14:47 +0100

    testing

--- src/src/SocketAddress.cpp
+++ src/src/SocketAddress.cpp
@@ -130,28 +130,43 @@ std::optional<std::string> toHost(const SocketAddress &address) {
   WSALazyInitializer::lazyInit();
 #endif
 
-  // refer to
-  // https://stackoverflow.com/questions/11684008/how-do-you-cast-sockaddr-structure-to-a-sockaddr-in-c-networking-sockets-ubu
+  std::optional<std::string> result;
   switch (address.sa_family) {
   case AF_INET: {
-    // ipv4 address
-    // inet_ntoa is deprecated, but using inet_ntop for ipv4 address, leads to
-    // an ip that has no sense
-    return std::string(::inet_ntoa(
-        reinterpret_cast<const SocketAddressIpv4 &>(address).sin_addr));
-
+    {
+      const auto &as_addr_in =
+          reinterpret_cast<const SocketAddressIpv4 &>(address).sin_addr;
+      char host_converted[INET_ADDRSTRLEN];
+      ::memset(&host_converted[0], 0, INET_ADDRSTRLEN);
+      const char *result_raw = ::inet_ntop(AF_INET, &as_addr_in,
+                                           &host_converted[0], INET_ADDRSTRLEN);
+      if (NULL != result_raw) {
+        result.emplace(result_raw);
+        break;
+      }
+    }
+    {
+      // try also inet_ntoa
+      result.emplace(::inet_ntoa(
+          reinterpret_cast<const SocketAddressIpv4 &>(address).sin_addr));
+      if ("0" == result.value()) {
+        result.reset();
+      }
+    }
   } break;
   case AF_INET6: {
-    // ipv6 address
-    char temp[INET6_ADDRSTRLEN]; // this is the longest one
-    // refer to
-    // https://www.gnu.org/software/libc/manual/html_node/Host-Address-Functions.html
-    ::memset(temp, 0, INET6_ADDRSTRLEN);
-    ::inet_ntop(address.sa_family, &address, temp, INET6_ADDRSTRLEN);
-    return std::string(temp, INET6_ADDRSTRLEN);
+    const auto &as_addr_in_6 =
+        reinterpret_cast<const SocketAddressIpv6 &>(address).sin6_addr;
+    char host_converted[INET6_ADDRSTRLEN];
+    ::memset(&host_converted[0], 0, INET6_ADDRSTRLEN);
+    const char *result_raw = ::inet_ntop(AF_INET6, &as_addr_in_6,
+                                         &host_converted[0], INET6_ADDRSTRLEN);
+    if (NULL != result_raw) {
+      result.emplace(result_raw);
+    }
   } break;
   }
-  return std::nullopt;
+  return result;
 }
 
 Address toAddress(const SocketAddress &address) {
