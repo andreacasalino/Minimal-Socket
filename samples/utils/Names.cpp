75d5ff1ec8309e5944d42ef3ff675e7156a8eff1
AndreaCasalino <andrea.casalino@cloudnc.com>
2022-05-14 15:20:52 +0100

    samples

--- samples/utils/Names.cpp
+++ samples/utils/Names.cpp
@@ -6,77 +6,18 @@
  **/
 
 #include <Names.h>
-#include <sstream>
 
-namespace sck::sample {
-    NamesMap::NamesMap() {
-        this->cursor = namesSurnames.begin();
-    }
+namespace MinimalSocket::test {
+Names NAMES_SURNAMES = {{"Luciano", "Pavarotti"},
+                        {"Gengis", "Khan"},
+                        {"Giulio", "Cesare"},
+                        {"Theodor", "Roosvelt"},
+                        {"Immanuel", "Kant"}};
 
-    const std::map<std::string, std::string> NamesMap::namesSurnames = {
-        {"Luciano", "Pavarotti"},
-        {"Gengis", "Khan"},
-        {"Giulio", "Cesare"},
-        {"Theodor", "Roosvelt"},
-        {"Immanuel", "Kant"}
-    };
-
-    const std::string NamesMap::unknown = "unknown";
-
-    NamesMap& NamesMap::operator++() {
-        ++this->cursor;
-        if (this->cursor == namesSurnames.end()) {
-            this->cursor = namesSurnames.begin();
-        }
-        return *this;
-    }
-
-    const std::string& NamesMap::getSurname(const std::string& name) {
-        auto it = namesSurnames.find(name);
-        if (it == namesSurnames.end()) return unknown;
-        return it->second;
-    }
-
-#ifdef TYPED_ENABLED
-    bool NamesDecoder::decode(const std::string& buffer, NamesCollection& message) const {
-        message.clear();
-        if (buffer.empty()) {
-            return true;
-        }
-        std::list<std::size_t> posSeparator;
-        for (std::size_t k = 0; k < buffer.size(); ++k) {
-            if (';' == buffer[k]) {
-                posSeparator.push_back(k);
-            }
-        }
-        if (posSeparator.empty()) {
-            message = { buffer };
-            return true;
-        }
-        std::size_t posPrev = 0;
-        for (auto it = posSeparator.begin(); it != posSeparator.end(); ++it) {
-            message.push_back(std::string(buffer, posPrev, *it - posPrev));
-            posPrev = *it + 1;
-        }
-        if (posPrev < buffer.size()) {
-            message.push_back(std::string(buffer, posPrev));
-        }
-        return true;
-    }
-
-    bool NamesEncoder::encode(std::string& buffer, const NamesCollection& message) const {
-        std::stringstream stream;
-        if (!message.empty()) {
-            auto it = message.begin();
-            stream << *it;
-            ++it;
-            for (it; it != message.end(); ++it) {
-                stream << ';';
-                stream << *it;
-            }
-        }
-        buffer = stream.str();
-        return true;
-    }
-#endif
+void NamesCircularIterator::next() {
+  ++current_;
+  if (current_ == NAMES_SURNAMES.end()) {
+    current_ = NAMES_SURNAMES.begin();
+  }
 }
+} // namespace MinimalSocket::test
--- samples/utils/Names.h
+++ samples/utils/Names.h
@@ -5,51 +5,26 @@
  * report any bug to andrecasa91@gmail.com.
  **/
 
-#ifndef SAMPLE_NAMES_H
-#define SAMPLE_NAMES_H
+#pragma once
 
-#include <map>
 #include <string>
-#ifdef TYPED_ENABLED
-#include <core/components/Encoder.h>
-#include <core/components/Decoder.h>
-#include <list>
-#endif
+#include <unordered_map>
 
-namespace sck::sample {
-    class NamesMap {
-    public:
-        NamesMap();
+namespace MinimalSocket::test {
+using Names = std::unordered_multimap<std::string, std::string>;
+static const Names NAMES_SURNAMES;
 
-        inline const std::string& getCursorName() const { return this->cursor->first; };
-        inline const std::string& getCursorSurname() const { return this->cursor->second; };
+using NamesIterator = Names::const_iterator;
 
-        static const std::string& getSurname(const std::string& name);
+class NamesCircularIterator {
+public:
+  NamesCircularIterator() : current_(NAMES_SURNAMES.begin()){};
 
-        NamesMap& operator++();
+  const NamesIterator &current() { return current_; };
 
-        inline static std::size_t size() { return namesSurnames.size(); };
+  void next();
 
-    private:
-        static const std::map<std::string, std::string> namesSurnames;
-        static const std::string unknown;
-
-        std::map<std::string, std::string>::const_iterator cursor;
-    };
-
-#ifdef TYPED_ENABLED
-    typedef std::list<std::string> NamesCollection;
-
-    class NamesDecoder : public typed::Decoder<NamesCollection> {
-    protected:
-        bool decode(const std::string& buffer, NamesCollection& message) const final;
-    };
-
-    class NamesEncoder : public typed::Encoder<NamesCollection> {
-    protected:
-        bool encode(std::string& buffer, const NamesCollection& message) const final;
-    };
-#endif
-}
-
-#endif
+private:
+  NamesIterator current_;
+};
+} // namespace MinimalSocket::test
