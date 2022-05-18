43693007b706af6fded22ad8403db0b39379948a
AndreaCasalino <andrea.casalino@cloudnc.com>
2022-05-18 11:26:21 +0100

    documenting

--- samples/udp/UdpAsker.cpp
+++ samples/udp/UdpAsker.cpp
@@ -5,8 +5,14 @@
  * report any bug to andrecasa91@gmail.com.
  **/
 
+///////////////////////////////////////////////////////////////////////////
+//                     Have a look to README.md                          //
+///////////////////////////////////////////////////////////////////////////
+
+// elements from the MinimalSocket library
 #include <MinimalSocket/udp/UdpSocket.h>
 
+// just a bunch of utilities
 #include <Args.h>
 #include <Ask.h>
 
@@ -26,8 +32,9 @@ int main(const int argc, const char **argv) {
 
   const MinimalSocket::Address remote_address(remote_host, remote_port);
   MinimalSocket::udp::UdpBinded asker(port_this, remote_address.getFamily());
- 
-  std::this_thread::sleep_for(std::chrono::seconds{1}); // just to be sure the responder has already prepared the receive
+
+  std::this_thread::sleep_for(std::chrono::seconds{
+      1}); // just to be sure the responder has already prepared the receive
   if (!asker.open()) {
     cout << "Failed to reserve specified port" << endl;
     return EXIT_FAILURE;
