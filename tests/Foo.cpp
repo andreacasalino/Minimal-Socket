d7fbba5dc6256d2a96c614bd26b63341b5a3071a
Foo <foo:gmail.com>
2023-05-29 15:52:48 +0200

    test with no omp

--- tests/Foo.cpp
+++ tests/Foo.cpp
@@ -8,13 +8,13 @@
 TEST_CASE("foo", "[foo]") {
   std::mutex cout_mtx;
 
-#pragma omp parallel num_threads(2)
-  {
-    std::scoped_lock lk(cout_mtx);
-    if (0 == omp_get_thread_num()) {
-      std::cout << "from main" << std::endl;
-    } else {
-      std::cout << "from slave" << std::endl;
-    }
-  }
+  // #pragma omp parallel num_threads(2)
+  //   {
+  //     std::scoped_lock lk(cout_mtx);
+  //     if (0 == omp_get_thread_num()) {
+  //       std::cout << "from main" << std::endl;
+  //     } else {
+  //       std::cout << "from slave" << std::endl;
+  //     }
+  //   }
 }
