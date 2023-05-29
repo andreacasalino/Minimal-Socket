d96864fdbc20def628ddf8198de614a212453f96
Foo <foo:gmail.com>
2023-05-29 16:39:00 +0200

    clang 12

--- tests/Foo.cpp
+++ tests/Foo.cpp
@@ -3,18 +3,18 @@
 
 #include <iostream>
 #include <mutex>
-// #include <omp.h>
+#include <omp.h>
 
 TEST_CASE("foo", "[foo]") {
   std::mutex cout_mtx;
 
-  // #pragma omp parallel num_threads(2)
-  //   {
-  //     std::scoped_lock lk(cout_mtx);
-  //     if (0 == omp_get_thread_num()) {
-  //       std::cout << "from main" << std::endl;
-  //     } else {
-  //       std::cout << "from slave" << std::endl;
-  //     }
-  //   }
+#pragma omp parallel num_threads(2)
+  {
+    std::scoped_lock lk(cout_mtx);
+    if (0 == omp_get_thread_num()) {
+      std::cout << "from main" << std::endl;
+    } else {
+      std::cout << "from slave" << std::endl;
+    }
+  }
 }
