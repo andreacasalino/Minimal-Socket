a9858b01e91f6298c0622f3c9fb0d2f0053fab00
Foo <foo:gmail.com>
2023-05-29 15:45:36 +0200

    test

---
+++ tests/Foo.cpp
@@ -0,0 +1,20 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+
+#include <iostream>
+#include <mutex>
+#include <omp.h>
+
+TEST_CASE("foo", "[address]") {
+  std::mutex cout_mtx;
+
+#pragma omp parallel num_threads(2)
+  {
+    std::scoped_lock lk(cout_mtx);
+    if (0 == omp_get_thread_num()) {
+      std::cout << "from main" << std::endl;
+    } else {
+      std::cout << "from slave" << std::endl;
+    }
+  }
+}
