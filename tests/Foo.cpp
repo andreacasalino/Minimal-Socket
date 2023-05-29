1cb529a742b46b0d1a8e24acac223655c6a30341
Foo <foo:gmail.com>
2023-05-29 17:20:18 +0200

    clang pipeline on

--- tests/Foo.cpp
+++ tests/Foo.cpp
@@ -2,5 +2,14 @@
 #include <catch2/generators/catch_generators.hpp>
 
 #include <barrier>
+#include <thread>
 
-TEST_CASE("foo", "[foo]") { std::barrier br{2}; }
+TEST_CASE("foo", "[foo]") {
+  std::barrier br{2};
+
+  std::thread t([&br]() { br.arrive_and_wait(); });
+
+  br.arrive_and_wait();
+
+  t.join();
+}
