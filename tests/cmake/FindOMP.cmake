08817157797bdcd2774fc85f4b13dda8dd658242
Foo <foo:gmail.com>
2023-05-29 15:08:28 +0200

    minor

---
+++ tests/cmake/FindOMP.cmake
@@ -0,0 +1,16 @@
+function(Find_OpenMP)
+
+message(STATUS "looking for OpenMP")
+find_package(OpenMP)
+if(NOT TARGET OpenMP::OpenMP_CXX)
+    message(STATUS "not found: trying plan b")
+    find_package(Threads REQUIRED)
+    add_library(OpenMP::OpenMP_CXX IMPORTED INTERFACE)
+    set_property(TARGET OpenMP::OpenMP_CXX
+                 PROPERTY INTERFACE_COMPILE_OPTIONS ${OpenMP_CXX_FLAGS})
+    # Only works if the same flag is passed to the linker; use CMake 3.9+ otherwise (Intel, AppleClang)
+    set_property(TARGET OpenMP::OpenMP_CXX
+                 PROPERTY INTERFACE_LINK_LIBRARIES ${OpenMP_CXX_FLAGS} Threads::Threads)
+endif()
+
+endfunction()
