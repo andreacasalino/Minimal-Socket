f74355ef7140676436eb468f8423ae64b01e5541
Andrea <andrecasa91@gmail.com>
2024-02-19 22:05:23 +0000

    avoid custom target command in VisualStudio in favour of a Popen C++ wrapper

--- samples/cmake/MakeSample.cmake
+++ samples/cmake/MakeSample.cmake
@@ -20,6 +20,17 @@ function(MakeSample INPUT PREFIX)
 		set(BIN_LOCATION ${BIN_LOCATION}/$<CONFIG>)
 	endif()
 
+if (CMAKE_GENERATOR MATCHES "Visual Studio" OR BUILD_MinimalCppSocket_AVOID_CUSTOM_TARGET)
+	# add_custom_target is not supported by visual studio
+	add_executable(${TARGET_NAME} ${LauncherSource})
+	target_compile_definitions(${TARGET_NAME} PUBLIC
+	-D CMD="${Python3_EXECUTABLE} ${SCRIPT_LOCATION} --cmd ${CMAKE_CURRENT_SOURCE_DIR}/${INPUT}|${Python3_EXECUTABLE} ${SCRIPT_LOCATION} --location ${BIN_LOCATION} --cmd ${CMAKE_CURRENT_SOURCE_DIR}/${INPUT} --dest ${CMAKE_CURRENT_BINARY_DIR}/report.html --sleep 0.5"
+	)
+else()
+	add_custom_target(${TARGET_NAME}-preamble ALL 
+		COMMAND "${Python3_EXECUTABLE}" ${SCRIPT_LOCATION}
+		"--cmd" "${CMAKE_CURRENT_SOURCE_DIR}/${INPUT}"
+	)
     add_custom_target(${TARGET_NAME} ALL 
         COMMAND "${Python3_EXECUTABLE}" ${SCRIPT_LOCATION}
         "--location" "${BIN_LOCATION}"
@@ -27,4 +38,7 @@ function(MakeSample INPUT PREFIX)
         "--dest" "${CMAKE_CURRENT_BINARY_DIR}/report.html"
         "--sleep" "0.5"
     )
+	add_dependencies(${TARGET_NAME} ${TARGET_NAME}-preamble)
+endif()
+	
 endfunction()
